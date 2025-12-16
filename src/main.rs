fd8fbf04492421da6e861b425325a8dd965dfd0e
Diego Asterio <diegoasterio.z@gmail.com>
2025-12-16 08:04:14 +0100

    Day three first puzzle

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 
 mod day_one;
 mod day_two;
+mod day_three;
 
 fn get_content_from_file(file_path: String) -> String{
     println!("Reading file path: {file_path}");
@@ -24,6 +25,7 @@ fn main() {
         ("1", "2") => day_one::solve_snd(content),
         ("2", "1") => day_two::solve_fst(content),
         ("2", "2") => day_two::solve_snd(content),
+        ("3", "1") => day_three::solve_fst(content),
         _ => "No solution for day={day} and puzzle={puzzle}".to_string(),
     };
 
