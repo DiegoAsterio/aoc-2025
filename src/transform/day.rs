81eac53d6a3a979b8dd069780e09f65d04b020a9
Diego Asterio <diegoasterio.z@gmail.com>
2026-01-18 19:15:02 +0100

    Add solution for fourth day.

--- src/transform/day.rs
+++ src/transform/day.rs
@@ -3,12 +3,14 @@ use crate::{extract::input::PuzzleInput, load::output::PuzzleOutput};
 mod one;
 mod two;
 mod three;
+mod four;
 
 pub fn solve_puzzle(input: &PuzzleInput) -> Result<PuzzleOutput, String> {
     match input.day {
         1 => one::solve(input),
         2 => two::solve(input),
         3 => three::solve(input),
+        4 => four::solve(input),
         n => Err(format!("No solution available for day {number}", number=n).to_string())
     }
 }
